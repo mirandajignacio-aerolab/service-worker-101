e614454ab61ea946c0b82d86155284c66cc32af7
mirandajignacio-aerolab <74968188+mirandajignacio-aerolab@users.noreply.github.com>
2020-12-14 16:32:08 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Service Worker 101</h1>
+      <img
+        alt="kadabra"
+        src="https://img.pokemondb.net/sprites/black-white/normal/kadabra.png"
+      />
+      <img
+        alt="pikachu"
+        src="https://img.pokemondb.net/sprites/black-white/normal/pikachu.png"
+      />
+      <img
+        alt="squirtle"
+        src="https://img.pokemondb.net/sprites/black-white/normal/squirtle.png"
+      />
+      <img
+        alt="jigglypuff"
+        src="https://img.pokemondb.net/sprites/black-white/normal/jigglypuff.png"
+      />
+      <img
+        alt="charmander"
+        src="https://img.pokemondb.net/sprites/black-white/normal/charmander.png"
+      />
+      <img
+        alt="chikorita"
+        src="https://img.pokemondb.net/sprites/black-white/normal/chikorita.png"
+      />
+      <img
+        alt="machamp"
+        src="https://img.pokemondb.net/sprites/black-white/normal/machamp.png"
+      />
+    </div>
+  );
+}
